1d5f736949228a3d785474bb24d9facb74cd8988
Andrey Bataev <bataevai@gmail.com>
2022-12-25 13:31:40 +0400

    Exc.19

--- exc.19/Program.cs
+++ exc.19/Program.cs
@@ -6,5 +6,32 @@
 14212 -> нет
 12821 -> да
 23432 -> да
-
 */
+
+int number = 1;
+
+Console.WriteLine("Введите пятизначное число, а мы проверим является ли оно полиндромом");
+
+while(number > 99999 || number < 10000) {
+
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("необходимо ввести число от 10 000 до 99 999");
+
+}
+
+Console.WriteLine($"number/10000 {number / 10000} ");
+Console.WriteLine($"number/10 {number % 10} ");
+Console.WriteLine($"number/1000 {(number/1000) % 10} ");
+Console.WriteLine($"number/10000 {(number/10) % 10} ");
+
+if (number/10000 == number % 10 && (number/1000) % 10 == (number/10) % 10) {
+Console.WriteLine($"Число {number} является полиндромом");
+} 
+
+else Console.WriteLine($"Число {number} не является полиндромом");
+
+
+
+
+
