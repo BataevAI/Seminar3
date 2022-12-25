5bdf441390912d07c94d6506a699c0fb2284c875
Andrey Bataev <bataevai@gmail.com>
2022-12-25 14:25:46 +0400

    exc.23

--- exc.23/Program.cs
+++ exc.23/Program.cs
@@ -5,4 +5,23 @@
 
 */
 
+Console.WriteLine("Введите число N > 0, а мы выведем значения кубов числе от 1 до N");
+
+int N = 0;
+
+while(N < 1) {
+
+N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1) Console.WriteLine("необходимо ввести число N > 0");
+
+}
+
+for (int i = 1; i <= N; i ++) {
+
+if (i == 1) Console.Write($"Значения кубов: {i}");
+
+else Console.Write($", {Math.Pow(i, 3)}");
+
+}
 
